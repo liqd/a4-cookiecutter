c207d272beecccdf86cfaeddeb5247474ed7459a
fuzzylogic2000 <katharina.albers@liqd.de>
2018-06-14 12:38:50 +0200

    add userindicator

--- {{cookiecutter.project_name}}/apps/users/migrations/0002_add_notification.py
+++ {{cookiecutter.project_name}}/apps/users/migrations/0002_add_notification.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('ca_users', '0001_initial'),
+        ('{{cookiecutter.project_app_prefix}}_users', '0001_initial'),
     ]
 
     operations = [
