3376a3c0f8f981f0b257447bd936230294b7b170
Phillippa <p.morland@liqd.net>
2019-03-14 12:09:22 +0100

    updating blueprint image and file

--- {{cookiecutter.project_name}}/apps/dashboard/blueprints.py
+++ {{cookiecutter.project_name}}/apps/dashboard/blueprints.py
@@ -30,7 +30,7 @@ blueprints = [
          image='images/map-brainstorming.svg',
          settings_model=('a4maps', 'AreaSettings'),
      )),
-     ('poll',
+    ('poll',
      ProjectBlueprint(
          title=_('Poll'),
          description=_('Run customizable, multi-step polls detailed opinions'
@@ -40,13 +40,5 @@ blueprints = [
          ],
          image='images/poll.png',
          settings_model=None,
-         requirements=Requirements(
-             aims=[Aim.run_survey],
-             results=[Result.majority_vote],
-             experience=Experience.no_projects,
-             motivation=Motivation.not_found
-         ),
-         complexity=COMPLEXITY_VECTOR_E,
-         type=BlueprintNames.a4_poll.name
-))
+    ))
 ]
